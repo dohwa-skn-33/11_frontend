1719017242775b1e356f19ff88f99581ee5a40a0
DoHwa <124579018+KangDohwa@users.noreply.github.com>
2026-09-04 12:49:21 +0900

    260904_day2-07

--- day2-javascript/07_arrow_callbacks.js
+++ day2-javascript/07_arrow_callbacks.js
@@ -1,6 +1,10 @@
+// 이름이 있는 함수
 function multiplyNamed(left, right) {
   return left * right;
 }
+
+// 익명 함수를 변수에 대입
+// -> 변수명 == 함수명
 const multiplyExpression = function (left, right) {
   return left * right;
 };
@@ -11,7 +15,7 @@ const sayHello = () => "안녕하세요";
 const double = (number) => number * 2;
 let multiplyArrow;
 // ***** TODO 1. 두 매개변수를 곱해 반환하는 화살표 함수 전체를 저장하기 *****
-
+multiplyArrow = (left, right) => left * right;
 // **************************************************************
 console.log("매개변수 0개:", sayHello());
 console.log("매개변수 1개:", double(4));
@@ -21,11 +25,14 @@ if (typeof multiplyArrow === "function") {
   console.log("두 매개변수 함수 값:", multiplyArrow);
 }
 
+// 식 본문이 포함된 화살표 함수
 const squareExpression = (number) => number * number;
+
+// 블록 본문이 포함된 화살표 함수
 const squareBlock = (number) => {
   const result = number * number;
   // ***** TODO 2. 블록 형태의 화살표 함수에서 result 반환하기 *****
-
+  return result;
   // **************************************************************
 };
 const squareWithoutReturn = (number) => {
@@ -35,9 +42,19 @@ console.log("식의 자동 반환:", squareExpression(5));
 console.log("블록의 명시적 반환:", squareBlock(5));
 console.log("return 없는 블록:", squareWithoutReturn(5));
 
+/* 고차 함수
+ - 함수를 매개 변수로 전달받거나, 함수를 반환하는 함수
+ */
+
+/* 매개 변수 operation
+ - addTen 함수를 전달받음
+  == 콜백 함수
+    -> 고차 함수 호출(call)시 매개 변수로 전달된 함수
+       고차 함수에서 내에서 해당 함수를 이용해서 결과를 만들어 반환(back)
+ */
 function applyOperation(value, operation) {
   // ***** TODO 3. 전달받은 함수를 value로 호출하고 그 결과 반환하기 *****
-
+  return operation(value);
   // **************************************************************
 }
 const addTen = function (number) {
@@ -49,7 +66,7 @@ console.log("익명 함수로 전달:", applyOperation(5, function (number) {
 }));
 let callbackResult;
 // ***** TODO 4. 값을 두 배로 만드는 화살표 콜백을 applyOperation에 전달하기 *****
-
+callbackResult = applyOperation(5, (number) => number * 2);
 // **************************************************************
 console.log("화살표로 전달:", callbackResult);
 console.log("전달할 함수의 자료형:", typeof addTen);
